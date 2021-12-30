5653201bace3ba27cfdb6ecc98116adf7cdb6610
PrePan <1367544416@qq.com>
2021-12-30 09:11:52 +0800

    CloudMusicDownload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-layout id="components-layout-demo-custom-trigger">
+      <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+        <div class="logo">PrePan小助手</div>
+        <a-menu theme="dark" mode="inline">
+          <a-menu-item key="1" @click="cmd">
+            <a-icon type="customer-service" />
+            <span>网易云音频下载</span>
+          </a-menu-item>
+          <a-menu-item key="2" @click="mt">
+            <a-icon type="message" />
+            <span>组队小助手</span>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+              class="trigger"
+              :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+              @click="() => (collapsed = !collapsed)"
+          />
+        </a-layout-header>
+        <a-layout-content
+            :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+        >
+          <router-view></router-view>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+  methods: {
+    cmd(){
+      this.$router.push('/cloudmusicdownload')
+    },
+    mt(){
+      this.$router.push('/make-team')
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .logo{
+    font-size: 22px;
+    color: white;
+    background: none !important;
+    text-align: center;
+  }
+  #components-layout-demo-custom-trigger .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+  }
+
+  #components-layout-demo-custom-trigger .trigger:hover {
+    color: #1890ff;
+  }
+
+  #components-layout-demo-custom-trigger .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+import VueRouter from 'vue-router'
+import 'ant-design-vue/dist/antd.css';
+import router from "./router";
 
 Vue.config.productionTip = false
 
+Vue.use(Antd);
+Vue.use(VueRouter)
+
+
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
