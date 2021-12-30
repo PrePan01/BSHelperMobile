a1236cc18cb1001a9cbb1eefd5d1e59e6834b9a3
PrePan <1367544416@qq.com>
2021-12-30 09:14:29 +0800

    Empty

---
+++ src/components/MakeTeam.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>敬请期待</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MakeTeam"
+}
+</script>
+
+<style scoped>
+  h1{
+    font-size: 30px;
+    color: black;
+  }
+</style>
