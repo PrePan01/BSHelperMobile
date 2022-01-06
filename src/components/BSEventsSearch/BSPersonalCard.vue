55db2c28deacaf94246f3063d3b3feaf60d1e551
PrePan <1367544416@qq.com>
2022-01-07 01:32:24 +0800

    BS个人资料

---
+++ src/components/BSEventsSearch/BSPersonalCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <!--标签搜索框-->
+    <div>
+      <a-input-search
+          prefix="#"
+          placeholder="请输入个人标签"
+          enter-button="查询"
+          size="large"
+          @search="onSearch"
+          v-model="searchInput"
+          style="width: 20vw;"
+          @keyup="upperCase"
+      />
+    </div>
+
+    <p>{{playerInfo}}</p>
+    <!--个人数据卡片-->
+    <a-card style="width: 70vw">
+
+    </a-card>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "BSPersonalCard",
+  data(){
+    return{
+      searchInput: '',  //标签搜索框
+      playerInfo: {}
+    }
+  },
+  methods: {
+    //输入框输入字符转大写
+    upperCase(){
+      this.searchInput = this.searchInput.toUpperCase()
+    },
+    //获取个人信息数据
+    onSearch(){
+      axios({
+        methods: 'GET',
+        url: '/playStatsApi/' + this.searchInput,
+      }).then((res) => {
+        this.playerInfo = res
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
