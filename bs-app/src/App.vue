da5bf44c7d77d966b17c6535b7829f351e7df34e
PrePan <1367544416@qq.com>
2022-07-25 09:28:25 +0800

    add matomo

--- bs-app/src/App.vue
+++ bs-app/src/App.vue
@@ -7,6 +7,19 @@
 </template>
 
 <script>
+// matomo
+  var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="https://prepantop.matomo.cloud/";
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '1']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.async=true; g.src='//cdn.matomo.cloud/prepantop.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+// 百度统计
   var _hmt = _hmt || [];
   (function() {
     var hm = document.createElement("script");
